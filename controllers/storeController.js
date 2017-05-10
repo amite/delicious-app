ea0c37d429eb97c2739cc335f9d5c6137bbd36de
amite <amit.erandole@gmail.com>
2017-05-10 10:05:09 +0530

    added file uploads

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,38 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
+const multer = require('multer')
+const jimp = require('jimp')
+const uuid = require('uuid')
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed!' }, false);
+    }
+  }
+}
+
+exports.upload = multer(multerOptions).single('photo')
+
+exports.resize = async (req, res, next) => {
+  if(!req.file) {
+    console.log('NO FILE TO RESIZE')
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to our filesystem, keep going!
+  next();
+}
 
 exports.homePage = (req, res) => {
   res.render('index', {
@@ -30,9 +63,11 @@ exports.updateStore = async (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
+  console.log('FILE HERE', req.file)
   const store = await (new Store(req.body).save())
   req.flash('success', `Successfully created ${store.name}. Would you like to create a review?`)
-  res.redirect(`/store/${store.slug}`)
+  res.redirect('/')
+  // res.redirect(`/store/${store.slug}`)
 }
 
 exports.getStores = async (req, res) => {
